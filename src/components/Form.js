dd81a73d8e381b233bf7bf23e0eaf4979b8a7296
diman <diman63sss2@mail.ru>
2022-12-17 16:29:33 +0400

    init commit

---
+++ src/components/Form.js
@@ -0,0 +1,90 @@
+import React, {useCallback, useRef, useState} from 'react';
+import axios from "axios";
+
+const Form = () => {
+    const UPLOAD_AVATAR = 'http://localhost:8080/api/upload_avatar';
+    const fileRef = useRef(null);
+    const [ imgUrl1, setImgUrl1 ] = useState('');
+    const [ imgUrl2, setImgUrl2 ] = useState('');
+    const [ answer, setAnswer ] = useState(null);
+    const [ loading, setLoading ] = useState(false);
+
+    const myImgonChange1 = function(event) {
+        let target = event.target;
+
+        if (!FileReader) {
+            alert('FileReader не поддерживается — облом');
+            return;
+        }
+
+        if (!target.files.length) {
+            alert('Ничего не загружено');
+            return;
+        }
+
+        let fileReader = new FileReader();
+        fileReader.onload = function() {
+            setImgUrl1(fileReader.result);
+        }
+
+        fileReader.readAsDataURL(target.files[0]);
+    }
+
+    const myImgonChange2 = function(event) {
+        let target = event.target;
+
+        if (!FileReader) {
+            alert('FileReader не поддерживается — облом');
+            return;
+        }
+
+        if (!target.files.length) {
+            alert('Ничего не загружено');
+            return;
+        }
+
+        let fileReader = new FileReader();
+        fileReader.onload = function() {
+            setImgUrl2(fileReader.result);
+        }
+
+        fileReader.readAsDataURL(target.files[0]);
+    }
+
+    let url = 'http://localhost:8080/api/receive/VGG19';
+
+    const handleSubmit = async(event) => {
+        event.preventDefault()
+        let fileOriginal = imgUrl1;
+        let fileStyle = imgUrl2;
+        const formData = new FormData();
+        formData.append("original_image", fileOriginal);
+        formData.append("style_image", fileStyle);
+        const response = await axios({
+            method: "post",
+            url: url,
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
+        }).then(response => setAnswer(response));
+    }
+
+
+    return (
+        <form id="form" method="post"  onSubmit={handleSubmit}>
+            <input onChange={e =>  myImgonChange1(e)} type="file" id="image-input-to-style" accept="image/jpeg, image/png, image/jpg"/>
+            <div id="display-image-to-style">
+                <img src={imgUrl1} alt=""/>
+            </div>
+            <h4>Upload style image</h4>
+            <input onChange={e =>  myImgonChange2(e)} type="file" id="image-input-style" accept="image/jpeg, image/png, image/jpg"/>
+            <div id="display-image-style">
+                <img src={imgUrl2} alt=""/>
+            </div>
+            <button id="submit">
+                Отправить
+            </button>
+        </form>
+    );
+};
+
+export default Form;
